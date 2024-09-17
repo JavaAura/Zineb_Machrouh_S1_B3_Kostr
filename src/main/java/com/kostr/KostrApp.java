eef8dc3a262338c563a57475de777bb72fd8b359
zinebMachrouh <zinebmac49@gmail.com>
2024-09-17 21:49:54 +0100

    ConsoleUI menu initialization

--- src/main/java/com/kostr/KostrApp.java
+++ src/main/java/com/kostr/KostrApp.java
@@ -4,8 +4,11 @@ import main.java.com.kostr.config.DatabaseConnection;
 import main.java.com.kostr.ui.ConsoleUI;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class KostrApp {
-    Connection connection = DatabaseConnection.getConnection();
-    ConsoleUI consoleUI = new ConsoleUI(connection);
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DatabaseConnection.getConnection();
+        ConsoleUI consoleUI = new ConsoleUI(connection);
+    }
 }
--- src/main/java/com/kostr/ui/ConsoleUI.java
+++ src/main/java/com/kostr/ui/ConsoleUI.java
@@ -1,4 +1,22 @@
 package main.java.com.kostr.ui;
 
+import main.java.com.kostr.config.Session;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class ConsoleUI {
+    private final Connection connection;
+    private static Session session = Session.getInstance();
+
+
+    public ConsoleUI(Connection connection) throws SQLException {
+        this.connection = connection;
+
+        menu();
+    }
+
+    public void menu(){
+        System.out.println("Helooooooooooo Kostr!");
+    }
 }
